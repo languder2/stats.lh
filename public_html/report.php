ac69440db09fb41d503cc3e9d30f6f73e19d63e5
languder2 <163087417+languder2@users.noreply.github.com>
2024-06-10 16:32:36 +0300

    mssql -> mysql

---
+++ public_html/report.php
@@ -0,0 +1,12 @@
+<?php
+//require_once "src/config.php";
+//
+//require_once "src/mysql.php";
+
+
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
